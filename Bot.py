13a04c9b3114ba6c3391a3d3139fb1fbd8840ce7
dem0-0 <85748316+dem0-0@users.noreply.github.com>
2022-04-28 12:44:41 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -10,9 +10,11 @@ def help_command(update, context):
     update.message.reply_text("""/yardim => Bu mesaj                                                    \
                                  /kaynak => Kaynak kodlarını gör                                        \
                                  /sonBlock => Zincirdeki son bloğun ID'si                               \
+                                 /bakiye (adres) => Verilen adresin bakiyesi                            \
                                  /islemBilgisi (hashKod) => Hash Kodu girilen işlemin bilgileri         \
                                  /islemSayisi (blokId) => ID'si verilen bloğun içerdiği işlem sayısı    \
-                                 /tps (süre) => Verilen sürede gerçekleşen işlem sayısı               """)
+                                 /tii (süre) => Verilen süre aralığında gerçekleşen işlem sayısı        \
+                                 /tps (X) => Son 'x' blokta saniye başına düşen ortalama işlem sayısı """) 
 
 def sourceCode_command(update, context):
     update.message.reply_text("https://github.com/dem0-0/BSCScannerTelegramBot")
@@ -20,6 +22,9 @@ def sourceCode_command(update, context):
 def latestBlock_command(update,context):
     update.message.reply_text(BSCScan.getLatestBlock())
 
+def getBalance_command(update, context):
+    update.message.reply_text(BSCScan.getBalance(context.args[0]))
+    
 def transactionInfo_command(update, context):
     update.message.reply_text(BSCScan.getTransactionInfo(context.args[0]))
     
@@ -29,6 +34,8 @@ def transactionCount_command(update, context):
 def transactionInInterval_commdand(update, context):
     update.message.reply_text(BSCScan.transactionInInterval(int(context.args[0])))
 
+def transactionInLastX_command(update, context):
+    update.message.reply_text(BSCScan.tpsInLatestBlocks(context.args[0]))
 
 #Setting up the Bot
 def main():
@@ -38,11 +45,13 @@ def main():
     disp.add_handler(CommandHandler("basla", start_command))
     disp.add_handler(CommandHandler("yardim", help_command))
     disp.add_handler(CommandHandler("kaynak", sourceCode_command))
+    disp.add_handler(CommandHandler("bakiye", getBalance_command))
     disp.add_handler(CommandHandler("sonBlok", latestBlock_command))
     disp.add_handler(CommandHandler("islemBilgisi", transactionInfo_command))
     disp.add_handler(CommandHandler("islemSayisi", transactionCount_command))
-    disp.add_handler(CommandHandler("tps", transactionInInterval_commdand))
-    
+    disp.add_handler(CommandHandler("tii", transactionInInterval_commdand))
+    disp.add_handler(CommandHandler("tps", transactionInLastX_command))
+        
     updater.start_polling()
     updater.idle()
     
